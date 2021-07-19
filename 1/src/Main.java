b067cf914868dcc179f55b0c698c99878b75c4ac
USER <USER@DESKTOP-21GRHL9>
2021-07-19 11:15:27 +0900

    2021-07-19 test

---
+++ 1/src/Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+		int[][] arr1 = new int[2][4];
+
+		int[][] arr2 = new int[2][4];
+
+		int[][] arr3 = new int[2][4];
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("first array");
+
+		for (int i = 0; i < 2; i++) {
+
+			for (int j = 0; j < 4; j++) {
+
+				arr1[i][j] = sc.nextInt();
+
+			}
+
+		}
+
+		System.out.println("second array");
+
+		for (int i = 0; i < 2; i++) {
+
+			for (int j = 0; j < 4; j++) {
+
+				arr2[i][j] = sc.nextInt();
+
+			}
+
+		}
+
+		for (int i = 0; i < 2; i++) {
+
+			for (int j = 0; j < 4; j++) {
+
+				arr3[i][j] = arr1[i][j] * arr2[i][j];
+
+				System.out.print(arr3[i][j] + " ");
+
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+}
